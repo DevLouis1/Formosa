e4695b9f159a0bdf4292cb78aa0551369594bda4
Louis <siapaarvin@gmail.com>
2025-10-21 16:24:56 +0700

    first steps, TESTING PURPOSES

---
+++ src/Components/Intro.tsx
@@ -0,0 +1,9 @@
+
+import { JSX } from 'react/jsx-runtime';
+import './Intro.css'
+
+
+export default function INTRO(): JSX.Element
+{ return (
+<div className='IntroText'>This is testing</div>
+); }
